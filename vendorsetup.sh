70c34d53d2069906d38dbd3ea18a244cc3de2c72
Hritwik <hritwiks.int@media.net>
2022-07-08 22:42:53 +0530

    do nat log in final builds

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,4 @@ export USE_CCACHE=1
 export SKIP_ABI_CHECKS=true
 
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
+git revert 6ab36bd --no-edit
