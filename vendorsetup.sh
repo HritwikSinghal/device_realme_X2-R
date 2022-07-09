f29aac9937ec61292e1aec0e3aa1b2f6ea02b105
Hritwik <hritwiks.int@media.net>
2022-07-09 18:05:27 +0530

    add HALs

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,11 @@ export SKIP_ABI_CHECKS=true
 
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
 git revert 6ab36bd --no-edit
+
+rm -rf hardware/qcom-caf/sm8150/audio
+rm-rf hardware/qcom-caf/sm8150/display
+rm -rf hardware/qcom-caf/sm8150/media
+
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-19.1-caf-sm8150 hardware/qcom-caf/sm8150/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-19.1-caf-sm8150 hardware/qcom-caf/sm8150/display
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-19.1-caf-sm8150 hardware/qcom-caf/sm8150/media
